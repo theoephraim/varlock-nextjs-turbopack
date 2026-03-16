1b93ab53e2e24b0b14935f97ec18d809af8c9860
Theo Ephraim <theo@dmno.dev>
2026-03-16 02:42:25 -0700

    put back proxy

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,14 @@
 import type { NextRequest } from 'next/server';
-// import { ENV, getRedactionMapInfo, redactSensitiveConfig } from 'varlock/env';
+import { ENV } from 'varlock/env';
 
 export function proxy(_request: NextRequest) {
-  // console.log('proxy - log vars', {
-  //   SECRET_ITEM: ENV.SECRET_ITEM,
-  //   ENV_SPECIFIC_ITEM: ENV.ENV_SPECIFIC_ITEM,
-  //   NEXT_PUBLIC_VAR: ENV.NEXT_PUBLIC_VAR,
-  //   PUBLIC_VAR: ENV.PUBLIC_VAR,
-  //   VAR_FROM_UI: ENV.VAR_FROM_UI,
-  // });
+  console.log('proxy - log vars', {
+    SECRET_ITEM: ENV.SECRET_ITEM,
+    ENV_SPECIFIC_ITEM: ENV.ENV_SPECIFIC_ITEM,
+    NEXT_PUBLIC_VAR: ENV.NEXT_PUBLIC_VAR,
+    PUBLIC_VAR: ENV.PUBLIC_VAR,
+    VAR_FROM_UI: ENV.VAR_FROM_UI,
+  });
 }
 
 export const config = {
